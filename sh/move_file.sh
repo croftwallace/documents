52fa9f493bccb279400c7065a58004e79c9aa0bb
croftwallace <croft_wallace@163.com>
2020-11-03 11:15:04 +0800

    add shell

---
+++ sh/move_file.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+IFS=$'\n'
+base=$(cd `dirname $0`; pwd)
+
+for d in `find . -type d`; do
+    #echo $d
+    cd $base
+    #cd $d
+    #mv *.png *.PNG *.gif *.JPG *.jpg *.jpeg *.JPEG ../
+    name=$d
+    len=${#name}
+    #echo $name
+    #echo $len
+    if [ $len -gt 15 ]; then
+        echo $name
+        echo "length bigger 14"
+        cd $d
+        pwd
+        for i in ./*; do
+            mv $i ../; 
+        done
+    cd $base
+    rm -d $d
+    fi
+
+done
+echo 'move finished'
